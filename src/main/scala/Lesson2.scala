98ce131cc0813de9274255ec01827a292e5e76c3
Prince <prince777_98@mail.ru>
2021-09-26 01:14:49 +0500

    Lesson2: tasks

--- src/main/scala/Lesson2.scala
+++ src/main/scala/Lesson2.scala
@@ -10,20 +10,27 @@ object Lesson2 extends App {
 //      println(element)
 //    }
 //  }
-  val mapList = list1.map { element =>
-    if (element % 2 != 0 && element > 5 && element < 8) {
-      println(element)
-      element
-    } else {
-      0
-    }
-  }
-   println(s"mapList: $mapList")
+//  val mapList = list1.map { element =>
+//    if (element % 2 != 0 && element > 5 && element < 8) {
+//      println(element)
+//      element
+//    } else {
+//      0
+//    }
+//  }
+//   println(s"mapList: $mapList")
 //  val filtered = list1.filterNot(element => element % 2 != 0 && element > 5 && element < 8)
 //  println(filtered)
   val str1 = List("hello", "scala", "world", "enjoy")
-  val res1 =  ""// write code string contains letter "a"
-  val res2 = ""// write code string without  letter "l"
-  val res3 = ""// write code string with only 3 letters of words
+  val res1 =  str1.filter { element =>
+    element.contains("a")
+  }// write code string contains letter "a"
+  val res2 = str1.map { element =>
+    element.filter(el => el != 'l')
+  }// write code string without  letter "l"
+  val res3 = str1.map {element =>
+//    s"${element(0)}${element(1)}${element(2)}"
+    element.take(3)
+  }// write code string with only 3 letters of words
   println(s"res1: $res1\nres2: $res2\nres3: $res3")
 }
