af7410ed5adfefaf27cecdf4a9bdaf90bb1d5c9c
Prince <prince777_98@mail.ru>
2021-10-01 01:47:22 +0500

    Scala loops 2

--- src/main/scala/Lesson4.scala
+++ src/main/scala/Lesson4.scala
@@ -68,7 +68,71 @@ object Lesson4 extends App {
       if i > 3 && i < 7
     } println(s"current i: $i")
 
-  exampleFor7()
+  def exampleFor8(): Unit =
+    for {
+      i <- 0 until 10
+      j <- 0 until 10
+    } {
+      print(s"current i: $i | ")
+      println(s"current j: $j")
+    }
+
+  def exampleFor9(): Unit =
+    for(i <- 0 until 10 ) {
+      for (j <- 0 until i) {
+        print(s"current i: $i | ")
+        println(s"current j: $j")
+      }
+    }
+
+  def exampleFor10(): Unit =
+    for(i <- 0 until 10 ) {
+      for (j <- 0 until i) {
+        print(s"current i: $i | ")
+        println(s"current j: $j")
+      }
+    }
+
+  def exampleFor11(): Unit =
+    for((i, j) <- (0 until 16) zip (0 until 15)) {
+      print(s"current i: $i | ")
+      println(s"current j: $j")
+    }
+
+  def exampleFor12(): Unit =
+    for((n, i) <- (5 until 16).zipWithIndex) {
+      println(s"number: $n | index: $i")
+    }
+
+  def exampleFor13(): Unit = {
+    val numbers = for {
+      (n, i) <- (5 until 16).zipWithIndex
+      if n % 2 == 0
+    } yield {
+      println(s"number: $n | index: $i")
+      n
+    }
+
+    println(numbers)
+  }
+
+  def exampleForeach(): Unit = {
+    //(0 to 10).foreach(println)
+    //(0 to 10).foreach(n => println(n))
+//    (0 to 10).foreach(println(_))
+    val result: Unit = (0 to 10).foreach(n => n + 1)
+    println(result)
+  }
+
+  def exampleMap(): Unit = {
+    val result = (0 to 10).map(println)
+    //(0 to 10).map(n => println(n))
+    //(0 to 10).map(println(_))
+//    val result = (0 to 10).map(n => n * 2)
+    println(result)
+  }
+
+  exampleMap()
 
 
 
