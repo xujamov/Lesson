119915a6c3ebbb8816204da2be738d2bc6cd8e95
Surojiddin <isurojiddin@gmail.com>
2021-10-05 20:09:04 +0500

    func

--- src/main/scala/Lesson4Surojddin.scala
+++ src/main/scala/Lesson4Surojddin.scala
@@ -24,6 +24,30 @@ object Lesson4Surojddin extends App {
   }
 
 
+  def compareTriplets(a: Array[Int], b: Array[Int]): Array[Int] = {
+    // Write your code here
+    var alice = 0
+    var bob = 0
+
+    for(i <- 0 to 2) {
+      if (a(i) > b(i)) alice += 1
+      if (a(i) < b(i)) bob += 1
+    }
+
+    Array(alice, bob)
+
+  }
+
+  def staircase(n: Int) {
+    // Write your code here
+    for(i <- 1 to n){
+      println(" " * (n-1) + "n" * n)
+    }
+
+  }
+
+
+
 
 
 }
