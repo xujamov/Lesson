44eda19c0e4778fb705e4e9f25dba68076ee3e04
Behruz <behruz.ravshonbekovich@gmail.com>
2021-10-05 09:21:20 -0400

    Functions: initial commit

---
+++ src/main/scala/Functions.scala
@@ -0,0 +1,13 @@
+object Functions extends App {
+  def camelcase(s: String): Int = {
+    s.count(_.isUpper) + 1
+  }
+  //  println(camelcase("saveChangesInTheEditor"))
+
+//  def strongPass(password: String): Int = {
+//    // Return digit, symbol, lowerCase, uppperCase, length  counts
+//
+//  }
+
+
+}
