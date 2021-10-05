3f67987d5261babfec2a7a83057ae2b14731f995
Prince <prince777_98@mail.ru>
2021-10-06 00:51:37 +0500

    Functions: *

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -14,7 +14,10 @@ object Functions extends App {
 
 //  1.Task: 1 -> 10, 2 -> 20, 3 -> 30 (Int -> Int)
 //  2.Task: 1 -> 11, 2 -> 21, 3 -> 31 (Int -> Int)
-//  3.Task: 1 -> 11, 2 -> 21, 3 -> 31 (Int -> Int)
+//  3.Task: 1 -> 0, 2 -> 1, 3 -> 2 (Int -> Int)
+//  4.Task: "1" -> "10", "2" -> "20", "3" -> "30" (String -> String)
+//  5.Task: "1" -> 5, "2" -> 10, "3" -> 15 (String -> Int)
+//  6.Task: "00001" -> 10000, "00002" -> 20000, "0003" -> 3000 (String -> Int)
   //  println(camelcase("saveChangesInTheEditor"))
 
 //  def strongPass(password: String): Int = {
