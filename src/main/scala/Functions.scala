1a17ecaf0f4e6cefb6483b7163297401bb289bc0
Prince <prince777_98@mail.ru>
2021-10-06 02:19:54 +0500

    task9

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -5,12 +5,12 @@ object Functions extends App {
   def returnValue(a: String): String = {
     (a.toInt * 10).toString
   }
-  val result1 = returnValue(1)
-  val result2 = returnValue(2)
-  val result3 = returnValue(3)
-  println(s"Javob = $result1")
-  println(s"Javob = $result2")
-  println(s"Javob = $result3")
+//  val result1 = returnValue(1)
+//  val result2 = returnValue(2)
+//  val result3 = returnValue(3)
+//  println(s"Javob = $result1")
+//  println(s"Javob = $result2")
+//  println(s"Javob = $result3")
 
 //  1.Task: 1 -> 10, 2 -> 20, 3 -> 30 (Int -> Int)
 //  2.Task: 1 -> 11, 2 -> 21, 3 -> 31 (Int -> Int)
@@ -20,8 +20,17 @@ object Functions extends App {
 //  6.Task: "00001" -> 10000, "00002" -> 20000, "0003" -> 3000 (String -> Int)
 //  7.Task: (2,3) -> (4,5), (3,4) -> (5,6), (6,8) -> (8,10) ((Int, Int) -> (Int, Int))
 //  8.Task: (2,"3847238" -> 1, (3,"847737763") -> 2, (8,"837847388") -> 4 ((Int, String) -> Int)
-//  9.Task: "384" -> "3 - 1, 8 - 1, 4 - 1", "112" -> "1 - 2, 2 - 1", "4444" -> "4 - 4" (String -> String)
-//  10.Task: 33904 -> "33940", 400837 -> "483700", 800403 -> "843000" (Int -> String)
+//  9.Task: "384" -> "3 - 1, 8 - 1, 4 - 1", "112" -> "1 - 2, 2 - 1", "4444" -> "4 - 4"
+//  (String -> String)
+def task9(s: String): String ={
+  val ss = s.toCharArray.toSet.mkString
+  val res = for (i <- 0 until ss.length) yield {
+    s"${ss(i)} - ${s.count(_ == ss(i))}"
+  }
+  res.mkString(", ")
+}
+  println(task9("333888455"))
+  //  10.Task: 33904 -> "33940", 400837 -> "483700", 800403 -> "843000" (Int -> String)
   //  println(camelcase("saveChangesInTheEditor"))
 
 //  def strongPass(password: String): Int = {
