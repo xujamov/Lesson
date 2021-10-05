5932cd6926e38eb76b26d0cc29184f176e26549d
Prince <prince777_98@mail.ru>
2021-10-06 01:04:22 +0500

    Functions: *

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -2,8 +2,8 @@ object Functions extends App {
   def camelcase(s: String): Int = {
     s.count(_.isUpper) + 1
   }
-  def returnValue(a: Int): Int = {
-    a * 10
+  def returnValue(a: String): String = {
+    (a.toInt * 10).toString
   }
   val result1 = returnValue(1)
   val result2 = returnValue(2)
@@ -18,6 +18,10 @@ object Functions extends App {
 //  4.Task: "1" -> "10", "2" -> "20", "3" -> "30" (String -> String)
 //  5.Task: "1" -> 5, "2" -> 10, "3" -> 15 (String -> Int)
 //  6.Task: "00001" -> 10000, "00002" -> 20000, "0003" -> 3000 (String -> Int)
+//  7.Task: (2,3) -> (4,5), (3,4) -> (5,6), (6,8) -> (8,10) ((Int, Int) -> (Int, Int))
+//  8.Task: (2,"3847238" -> 1, (3,"847737763") -> 2, (8,"837847388") -> 4 ((Int, String) -> Int)
+//  9.Task: "384" -> "3 - 1, 8 - 1, 4 - 1", "112" -> "1 - 2, 2 - 1", "4444" -> "4 - 4" (String -> String)
+//  10.Task: 33904 -> "33940", 400837 -> "483700", 800403 -> "843000" (Int -> String)
   //  println(camelcase("saveChangesInTheEditor"))
 
 //  def strongPass(password: String): Int = {
