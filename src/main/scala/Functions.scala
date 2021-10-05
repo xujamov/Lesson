d7d763efe66e312819599d12f81d95748ac2a70c
Prince <prince777_98@mail.ru>
2021-10-06 00:47:15 +0500

    Functions: *

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -2,6 +2,19 @@ object Functions extends App {
   def camelcase(s: String): Int = {
     s.count(_.isUpper) + 1
   }
+  def returnValue(a: Int): Int = {
+    a * 10
+  }
+  val result1 = returnValue(1)
+  val result2 = returnValue(2)
+  val result3 = returnValue(3)
+  println(s"Javob = $result1")
+  println(s"Javob = $result2")
+  println(s"Javob = $result3")
+
+//  1.Task: 1 -> 10, 2 -> 20, 3 -> 30 (Int -> Int)
+//  2.Task: 1 -> 11, 2 -> 21, 3 -> 31 (Int -> Int)
+//  3.Task: 1 -> 11, 2 -> 21, 3 -> 31 (Int -> Int)
   //  println(camelcase("saveChangesInTheEditor"))
 
 //  def strongPass(password: String): Int = {
